3542174fa557da7ac8befb3377e458a55fc9075e
waaaly <825887013@qq.com>
2019-04-18 21:39:48 +0800

    添加消息中心页面

---
+++ search_test/pages/news/news.js
@@ -0,0 +1,96 @@
+// pages/news/news.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    processData: [{
+      name: '完成下单',
+      start: '#fff',
+      end: '#EFF3F6',
+      icon: '../../image/status.png'
+    },
+    {
+      name: '商家接单',
+      start: '#EFF3F6',
+      end: '#EFF3F6',
+      icon: '../../image/status-off.png'
+    },
+    {
+      name: '商家发货',
+      start: '#EFF3F6',
+      end: '#EFF3F6',
+      icon: '../../image/status-off.png'
+    },
+    {
+      name: '用户收货',
+      start: '#EFF3F6',
+      end: '#EFF3F6',
+      icon: '../../image/status-off.png'
+    },
+    {
+      name: '订单完成',
+      start: '#EFF3F6',
+      end: '#fff',
+      icon: '../../image/status-off.png'
+    }],
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
